d0955a15b8e69ec75cd5462c2fbf0903684692e3
JiwansOza <workjiwans@@gmail.com>
2025-07-09 21:49:09 +0530

    tried to implement mobile version

---
+++ android/app/src/main/java/com/jiwansoza/docconnect/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jiwansoza.docconnect;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jiwansoza.docconnect',
+  appName: 'DocConnect',
+  webDir: 'dist'
+};
+
+export default config;
